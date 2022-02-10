405752cafef9a423ada9bcc3ced384a1a1309766
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-10 10:48:18 +0530

    UC1_Create Board

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToe ticTacToe = new TicTacToe();
+            ticTacToe.BoardGrid();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TicTacToeGame/TicTacToe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    public class TicTacToe
+    {
+        public void BoardGrid()
+        {
+            char[] Grid = new char[10];
+            for (int i = 0; i<=10; i++)
+            {
+                Grid[i] = ' ';
+            } 
+        }
+    }
+}
